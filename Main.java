77e69e74857de1f3a04a7c8cb8f3b73eb1d9b847
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2023-09-29 11:56:09 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 package Preparazione;
 
 public class Main {
-
+    BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        System.out.println("Mail>_<??");
+    }
 }
